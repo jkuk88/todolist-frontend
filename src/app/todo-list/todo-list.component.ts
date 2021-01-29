d1978aaae7a700594610ebcfb7e7e5ccaca4e86d
József Kuk <jkuk@hotmail.hu>
2021-01-29 10:22:24 +0100

    ToDoListComponent development

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ToDoListItemModel } from '../models/todo-list-item.model';
+import { ToDoListItemService } from '../services/todo-list-item.service';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements OnInit, OnDestroy {
+  toDoListItems: ToDoListItemModel[] = [];
+  subscription!: Subscription;
+  filterDescription: string | null = null;
+  filterIsCompleted: boolean | null = null;
+  pageCount = 1;
+  pageNumber = 0;
 
-  constructor() { }
-
+  constructor(private todoListItemService: ToDoListItemService, private route: ActivatedRoute) { }
   ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        const p = params.page;
+        if (p == null) {
+          return;
+        }
+        this.pageNumber = p;
+        this.onFetchToDoListItems();
+      }
+    );
+
+    this.subscription = this.todoListItemService.itemsChanged.subscribe(
+      i => {
+        this.onFetchToDoListItems();
+      });
+
+    this.onFetchToDoListItems();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  onFetchToDoListItems(): void {
+    this.todoListItemService.fetchToDoItems(25, this.pageNumber, this.filterIsCompleted, this.filterDescription).subscribe(
+      result => {
+        this.toDoListItems = result.items;
+        this.pageCount = result.pageCount === 0 ? 1 : result.pageCount;
+      });
+  }
+
+  onNewToDoListItemClick(): void {
+    this.toDoListItems.push(new ToDoListItemModel(null, null, false, false));
+  }
+
+  statusFilterClicked(status: boolean | null, event: any): void {
+    const target = event.target;
+    if (target.checked) {
+      this.filterIsCompleted = status;
+      this.onFetchToDoListItems();
+    }
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backendUrl: 'https://localhost:44396/api/'
+  backendUrl: 'https://localhost:44353/api/'
 };
 
 /*
